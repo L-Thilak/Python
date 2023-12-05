66b1a6db7f80ca2bb7fb304caf55c4ed2e1352bd
L THILAK <141763716+L-Thilak@users.noreply.github.com>
2023-12-06 00:49:11 +0530

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,10 @@
+Example For Tuple:
+ # Creating a tuple 
+my_tuple = (1, 2, 3, 'a', 'b') 
+# Accessing elements
+ print(my_tuple[0]) 
+# Slicing 
+print(my_tuple[1:4]) # Output: (2, 3, 'a') 
+# Concatenation 
+new_tuple = my_tuple + (4, 5) 
+print(new_tuple) # Output: (1, 2, 3, 'a', 'b', 4, 5) 
